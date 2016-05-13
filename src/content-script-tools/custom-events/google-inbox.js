488cbc65b14fa8ee874b9405fb703cfc2874dda0
Daniel Perez <daniel@claudetech.com>
2016-05-14 07:10:59 +0900

    Fix inbox placeholder removal.

--- src/content-script-tools/custom-events/google-inbox.js
+++ src/content-script-tools/custom-events/google-inbox.js
@@ -5,9 +5,18 @@ export default {
   url: new RegExp('https://inbox\.google\.com.*', 'i'),
   // remove placeholder
   bind: function (window) {
+    const hideLabel = () => {
+      const label = this.elem.previousSibling;
+      if (!label || !label.tagName || label.tagName.toLowerCase() !== 'label') {
+        return;
+      }
+      label.innerText = '';
+      label.style.display = 'none';
+    };
+
     const handleValueSet = () => {
       if (this.getValue()) {
-        window.dispatchEvent(new Event('resize'));
+        hideLabel();
       } else {
         this.once('valueSet', handleValueSet);
       }
